ed9d7ac609982dcb538dac2e3a153ddbf76b36e1
Lightway <lightway29@gmail.com>
2017-04-22 01:56:36 +0530

    App flow and camera access updated

---
+++ SimpleTesseractExample-master/app/src/main/java/com/imperialsoupgmail/tesseractexample/CaptureImageActivity.java
@@ -0,0 +1,47 @@
+package com.imperialsoupgmail.tesseractexample;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class CaptureImageActivity extends AppCompatActivity {
+
+    ImageView result;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_image);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button click = (Button)findViewById(R.id.button4);
+        result = (ImageView)findViewById(R.id.imageView2);
+
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                result.setImageBitmap(imageBitmap);
+            }
+        }
+
+}
---
+++ SimpleTesseractExample-master/app/src/main/java/com/imperialsoupgmail/tesseractexample/MainScreenActivity.java
@@ -0,0 +1,44 @@
+package com.imperialsoupgmail.tesseractexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    Button btnCamera ;
+
+
+    public void init() {
+        btnCamera = (Button)findViewById(R.id.btnCamera);
+        btnCamera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openCaptureImage(v);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        init();
+
+    }
+
+    public void openCaptureImage(View view){
+        Intent myIntent = new Intent(this, CaptureImageActivity.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        this.startActivity(myIntent);
+    }
+
+}
