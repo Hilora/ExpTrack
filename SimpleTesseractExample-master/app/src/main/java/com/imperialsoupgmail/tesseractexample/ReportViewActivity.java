2ec747dd4997d0ad8effd4511f70619e9f9fa07f
Lightway <lightway29@gmail.com>
2017-12-02 08:24:28 +0530

    Minor fix

--- SimpleTesseractExample-master/app/src/main/java/com/imperialsoupgmail/tesseractexample/ReportViewActivity.java
+++ SimpleTesseractExample-master/app/src/main/java/com/imperialsoupgmail/tesseractexample/ReportViewActivity.java
@@ -1,11 +1,17 @@
 package com.imperialsoupgmail.tesseractexample;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.util.Log;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,7 +19,8 @@ import java.util.Date;
 
 public class ReportViewActivity extends AppCompatActivity {
 
-
+    static String fileName = "Monthly_Report.json";
+    String jsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +43,41 @@ public class ReportViewActivity extends AppCompatActivity {
 
         }
 
+        jsonString = getData(this);
+
+
+
         //Get the bundle
         Bundle bundle = getIntent().getExtras();
 
     }
 
-    void
+    public void saveData(Context context, String mJsonResponse) {
+        try {
+            FileWriter file = new FileWriter(context.getFilesDir().getPath() + "/" + fileName);
+            System.out.println("File path -----" + context.getFilesDir().getPath() + "/" + fileName);
+            file.write(mJsonResponse);
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            Log.e("TAG", "Error in Writing: " + e.getLocalizedMessage());
+        }
+    }
+
+    public String getData(Context context) {
+        try {
+            File f = new File(context.getFilesDir().getPath() + "/" + fileName);
+            //check whether file exists
+            FileInputStream is = new FileInputStream(f);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            return new String(buffer);
+        } catch (IOException e) {
+            Log.e("TAG", "Error in Reading: " + e.getLocalizedMessage());
+            return null;
+        }
+    }
 
 }
